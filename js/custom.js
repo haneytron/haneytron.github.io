1d9da68ed55aa636c560221eb3cea0aa9bf6ba6e
David Haney <david.a.haney@gmail.com>
2018-06-08 21:25:15 -0400

    Publishing my blog

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(function() {
+	$("span.prev-text.nav-default").text("Newer");
+	$("span.prev-text.nav-mobile").text("Newer");
+	$("span.next-text.nav-default").text("Older");
+	$("span.next-text.nav-mobile").text("Older");
+});
