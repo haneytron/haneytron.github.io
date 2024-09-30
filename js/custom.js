86dd0a4f2b53d181d16d5f1c1d8869d43766dbe3
David Haney <david.a.haney@gmail.com>
2024-09-30 11:46:43 -0400

    Cleaned up old files and folders in public blog folder and repo

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$("span.prev-text.nav-default").text("Newer");
-	$("span.prev-text.nav-mobile").text("Newer");
-	$("span.next-text.nav-default").text("Older");
-	$("span.next-text.nav-mobile").text("Older");
-});
